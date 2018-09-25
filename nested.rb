15e618d81f6f7d3defd9fc2a6b822ba349d40587
Austin Luft <ausluft@gmail.com>
2018-09-25 01:50:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  dennis_languages = programmer_hash[:dennis_ritchie][:languages]
+  dennis_languages << "Assembly"
+  return programmer_hash
 end
